9f5f77726cb99dcda059fc7517e506080dd7e180
ledo <lethanhtiendo345@gmail.com>
2026-03-24 14:10:05 +0700

    Upload

--- utils/uploadHandler.js
+++ utils/uploadHandler.js
@@ -13,14 +13,29 @@ let storage = multer.diskStorage({
     }
 })
 let filterImage = function (req, file, cb) {
+    console.log(file);
     if (file.mimetype.startsWith('image')) {
         cb(null, true)
     } else {
         cb(new Error("file khong dung dinh dang"))
     }
 }
-module.exports = multer({
-    storage: storage,
-    limits: 5 * 1024 * 1024,
-    fileFilter: filterImage
-})
+let filterExel = function (req, file, cb) {
+    if (file.mimetype.includes('spreadsheetml')) {
+        cb(null, true)
+    } else {
+        cb(new Error("file khong dung dinh dang"))
+    }
+}
+module.exports = {
+    uploadImage: multer({
+        storage: storage,
+        limits: 5 * 1024 * 1024,
+        fileFilter: filterImage
+    }),
+    uploadExcel: multer({
+        storage: storage,
+        limits: 5 * 1024 * 1024,
+        fileFilter: filterExel
+    })
+}
